7c966dd9bf1de4631a3b7269edc0c7c327312a6f
Andrea Epifani <andy.epifani@gmail.com>
2020-10-10 10:09:04 +1000

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,4 @@ exports.url							= require.resolve('url/');
 exports.util						= require.resolve('util/util.js');
 exports.vm							= require.resolve('vm-browserify');
 exports.zlib						= require.resolve('browserify-zlib');
+exports.async_hooks     = null;
